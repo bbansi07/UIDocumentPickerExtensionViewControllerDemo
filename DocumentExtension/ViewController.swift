325a2fb4e2c6c7a08fa2fb5977103efe424980f7
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-10-12 18:20:34 +0530

    Initial Commit

---
+++ DocumentExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DocumentExtension
+//
+//  Created by Zeitech Solutions on 12/10/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
